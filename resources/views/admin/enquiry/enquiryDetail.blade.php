448286f76cb91772d79eb720bb7bf1e289930700
Souravpj13 <meeniasourav123@gmail.com>
2020-04-17 16:03:44 +0530

    Updated

--- resources/views/admin/enquiry/enquiryDetail.blade.php
+++ resources/views/admin/enquiry/enquiryDetail.blade.php
@@ -17,16 +17,30 @@ td, th {
 }
 
 .enquiry-contain {
-    margin-left: 45%;
+    margin-left: 20px;
 }
+
 </style>
 </head>
 <br>
+
 <div class="enquiry-contain">
-<h4><strong>Enquiry Description</strong></h4>
+<div class="row">
+<div class="col-md-5" style="text-align:left">
+<a type="button" href="http://127.0.0.1:8000/admin/enquiry" style="font-family: Open Sans, sans-serif;color:white;width:100px;text-align:center;font-weight:bold; background-color:#60ACEF;" class="btn">Back</a>
+</div>
+<div class="col-md-7" style="text-align:left">
+<h4><strong>Enquiry Details</strong></h4>
+</div>
+</div>
 </div><hr><br>
+
 <div class="container" id="enquiry">
 
 </div>
+
+<script>
+$('textarea').autoResize();
+</script>
 
 @endsection
