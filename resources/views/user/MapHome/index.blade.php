7e5ddbb10148a7af273fc23beaeb2089314d0010
98ankit47 <57389607+98ankit47@users.noreply.github.com>
2020-04-20 18:26:13 +0530

    all

--- resources/views/user/MapHome/index.blade.php
+++ resources/views/user/MapHome/index.blade.php
@@ -5,24 +5,33 @@
         /* Always set the map height explicitly to define the size of the div
          * element that contains the map. */
         #map {
-          height: 100%;
+          height: 500px;
+          width:50%
         }
     
       </style>
-        <div id="map">
+        <div class="row">
+            <div id="map" class="col-md-6">
+            </div>
+            </div class="col-md-6">
 
+            </div>
         </div>
       
   
-        <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyD7ZAdsxYc_U1xxyA3ga9gcmG260tW783I"
-        async defer></script>
         <script>
-            $(document).ready(function(){
-                var map = new google.maps.Map(document.getElementById('map'), {
-                center: {lat: -34.397, lng: 150.644},
-                zoom: 8
-                });
-                alert(map);
-            });
-        </script>    
+             var map;
+            function initMap() {
+               // The location of Uluru
+                var house = {lat: -25.344, lng: 131.036};
+            // The map, centered at Uluru
+            var map = new google.maps.Map(
+            document.getElementById('map'), {zoom: 8, center: house});
+            // The marker, positioned at Uluru
+            var marker = new google.maps.Marker({position: house, map: map});
+               
+            }
+        </script>  
+        <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyD7ZAdsxYc_U1xxyA3ga9gcmG260tW783I&callback=initMap"
+          async defer></script>  
     @endsection
