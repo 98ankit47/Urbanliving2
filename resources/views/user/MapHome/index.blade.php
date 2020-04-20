01869d0ea2334b1da9e688bb9b349522e367c1ac
98ankit47 <57389607+98ankit47@users.noreply.github.com>
2020-04-20 20:55:22 +0530

    all

--- resources/views/user/MapHome/index.blade.php
+++ resources/views/user/MapHome/index.blade.php
@@ -21,17 +21,37 @@
   
         <script>
              var map;
+             var myLatLng={lat: 30.6792786, lng: 76.7269971};;
             function initMap() {
-               // The location of Uluru
-                var house = {lat: -25.344, lng: 131.036};
+               // The location of Uluru 
             // The map, centered at Uluru
             var map = new google.maps.Map(
-            document.getElementById('map'), {zoom: 8, center: house});
+            document.getElementById('map'), {zoom: 15, center: myLatLng});
             // The marker, positioned at Uluru
-            var marker = new google.maps.Marker({position: house, map: map});
+            var marker = new google.maps.Marker({position: myLatLng, map: map});
+
+            var request = {
+                location: myLatLng,
+                radius: '500',
+                type: ['store', 'school'],
+            };
+
+            var marker = new google.maps.Marker({
+                position: myLatLng,
+                map: map,
+                title: 'Hello World!'
+            });
+
+                service = new google.maps.places.PlacesService(map);
+                service.nearbySearch(request, callback);
+
+
+                function callback(results, status) {
+                    console.log(results);
+                }
                
             }
         </script>  
-        <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyD7ZAdsxYc_U1xxyA3ga9gcmG260tW783I&callback=initMap"
+        <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyD7ZAdsxYc_U1xxyA3ga9gcmG260tW783I&callback=initMap&libraries=places"
           async defer></script>  
     @endsection
