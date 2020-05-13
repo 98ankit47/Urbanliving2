a172ac038c443ff5376a2f241359554f6c475ea1
Souravpj13 <meeniasourav123@gmail.com>
2020-05-13 16:19:23 +0530

    Updates in neighbor

--- resources/views/user/neighbor/neighborHomes.blade.php
+++ resources/views/user/neighbor/neighborHomes.blade.php
@@ -1,5 +1,6 @@
 @extends('layouts.user') 
 @section('content')
+
 <style>
 #map {
           height: 500px;
@@ -13,22 +14,6 @@
         }
     
         .card .detail {
-        position: absolute;
-        top: 20%;
-        left: 50%;
-        transform: translate(-50%, -50%);
-        -ms-transform: translate(-50%, -50%);
-        width:70%;
-        color: white;
-        font-size: 18px;
-        padding: 8px 20px;
-        border-color: white;
-        cursor: pointer;
-        border-radius: 5px;
-        text-align: center;
-        }
-
-        .card .single {
         position: absolute;
         top: 40%;
         left: 50%;
@@ -44,7 +29,7 @@
         text-align: center;
         }
 
-        .card .town {
+        .card .summary {
         position: absolute;
         top: 60%;
         left: 50%;
@@ -54,22 +39,6 @@
         color: white;
         font-size: 18px;
         padding: 8px 20px;
-        border-color: white;
-        cursor: pointer;
-        border-radius: 5px;
-        text-align: center;
-        }
-
-        .card .mid{
-        position: absolute;
-        top: 80%;
-        left: 50%;
-        transform: translate(-50%, -50%);
-        -ms-transform: translate(-50%, -50%);
-        width:70%;
-        color: white;
-        font-size: 18px;
-        padding: 8px 20px;;
         border-color: white;
         cursor: pointer;
         border-radius: 5px;
@@ -95,24 +64,18 @@
         <div id="neighborHome">
             <div id="home1" class="card homebox1" style="width: 100%; height:24rem;" >
                 <img style="height:100%;" src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQkTHBpAyICO-H7DH0a6kYjGznn5y2WWRLuAw6PRn7QEkqfsuXt&usqp=CAU"/>
-                <a href="/neighborDetail" type="button" class="btn detail btn-outline-dark">Details</a>
-                <a href="/neighborHomes" type="button" class="btn single btn-outline-dark">Single Family</a>
-                <a href="/neighborHomes" type="button" class="btn town btn-outline-dark">TownHouse/Condo</a>
-                <a href="/neighborHomes" type="button" class="btn mid btn-outline-dark">Mid/Hi Rise Condo</a>
+                <a href="#" type="button" class="btn detail btn-outline-dark">Details</a>
+                <a href="#" type="button" class="btn summary btn-outline-dark">Summary</a>
             </div><br>
-            <div id="home3" class="card homebox1" style="width: 100%; height:24rem;" >
+            <div id="home2" class="card homebox1" style="width: 100%; height:24rem;" >
                 <img style="height:100%;" src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcT6FKg7LOZ32716WR_CzKNDh-DtZSKHNnWFwTxoxYjxms-SbBhU&usqp=CAU"/>
-                <a href="/neighborDetail" type="button" class="btn detail btn-outline-dark">Details</a>
-                <a href="/neighborHomes" type="button" class="btn single btn-outline-dark">Single Family</a>
-                <a href="/neighborHomes" type="button" class="btn town btn-outline-dark">TownHouse/Condo</a>
-                <a href="/neighborHomes" type="button" class="btn mid btn-outline-dark">Mid/Hi Rise Condo</a>
+                <a href="#" type="button" class="btn detail btn-outline-dark">Details</a>
+                <a href="#" type="button" class="btn summary btn-outline-dark">Summary</a>
             </div><br>
             <div id="home3" class="card homebox1" style="width: 100%; height:24rem;" >
                 <img style="height:100%;" src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcT0SD6h1f2t6g5HreAWg9zv2FV2BIZ-i39EcmfJ9VQCKSz_YgBm&usqp=CAU"/>
-                <a href="/neighborDetails" type="button" class="btn detail btn-outline-dark">Details</a>
-                <a href="/neighborHomes" type="button" class="btn single btn-outline-dark">Single Family</a>
-                <a href="/neighborHomes" type="button" class="btn town btn-outline-dark">TownHouse/Condo</a>
-                <a href="/neighborHomes" type="button" class="btn mid btn-outline-dark">Mid/Hi Rise Condo</a>
+                <a href="#" type="button" class="btn detail btn-outline-dark">Details</a>
+                <a href="#" type="button" class="btn summary btn-outline-dark">Summary</a>
             </div><br>
         </div>
     </div>
@@ -137,4 +100,5 @@ document.getElementById('btn').addEventListener('click', ev => {
   scrollIfNeeded(document.getElementById('home3'), document.getElementById('container'));
 });  
 </script>
+
 @endsection
