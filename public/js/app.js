ac3989df8a414db7c0c8685d1150f3f9df7c4eb9
Ankit <98ankit47@gmail.com>
2020-06-15 18:07:37 +0530

    NeighbourHome Page

--- public/js/app.js
+++ public/js/app.js
@@ -52868,8 +52868,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Users\HP\Documents\GitHub\Urbanliving2\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\Users\HP\Documents\GitHub\Urbanliving2\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\xampp\htdocs\UrbanLiving\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\xampp\htdocs\UrbanLiving\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
